ecb863a9890cfd974556bf5af2ee4bdb8d39fdb4
KCG WOLF <hr.karimi1385@gmail.com>
2025-08-05 16:03:45 +0330

    add to teacherClass

--- src/main/java/org/example/Teacher.java
+++ src/main/java/org/example/Teacher.java
@@ -1,11 +1,2 @@
 package org.example;
-public class Teacher {
-    private String firstName;
-    public String getFirstName() {
-        return firstName;
-    }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-}
