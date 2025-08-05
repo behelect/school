d833d25a9950d80835e2150546736a25eba5491c
KCG WOLF <hr.karimi1385@gmail.com>
2025-08-05 15:31:52 +0330

    add fild to teacherClass

--- src/main/java/org/example/Teacher.java
+++ src/main/java/org/example/Teacher.java
@@ -1,4 +1,23 @@
 package org.example;
 
 public class Teacher {
+    private String firstName;
+    private String lastName;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
 }
